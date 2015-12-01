9961ca3fe44eba7b7412f7fce39b4e1cd6b422c7
bvince <vi.berniere@gmail.com>
2015-12-01 14:03:18 +0100

    (queazy) add login/register/logout

--- queazy/app/AppKernel.php
+++ queazy/app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new GIL\QueazyBundle\GILQueazyBundle(),
+            new GIL\UserBundle\GILUserBundle(),
+            new FOS\UserBundle\FOSUserBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
