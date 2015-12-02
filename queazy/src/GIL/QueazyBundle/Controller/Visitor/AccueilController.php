9cc71e09dbc580e659d52f71887061617462ed17
Renan DECAMPS <renan.decamps@gmail.com>
2015-12-02 19:55:52 +0100

    Minor changes.

--- queazy/src/GIL/QueazyBundle/Controller/Visitor/AccueilController.php
+++ queazy/src/GIL/QueazyBundle/Controller/Visitor/AccueilController.php
@@ -9,8 +9,23 @@ class AccueilController extends Controller
     public function accueilAction()
     {
         // Affichage des quiz les plus récents
+        $em = $this->getDoctrine()->getManager();
+        $quizs = $em->getRepository('GILQueazyBundle:Quiz')->findAll();
 
+        $listeQuiz = array();
 
-        return $this->render('GILQueazyBundle:Visitor:accueil.html.twig');
+        foreach ($quizs as $quiz) {
+            $nbQuestions = count($em->getRepository('GILQueazyBundle:Question')->findBy(
+                array('quiz' => $quiz )));
+
+            array_push($listeQuiz, array(
+                'quiz' => $quiz,
+                'nb_questions' => $nbQuestions
+            ));
+        }
+
+        return $this->render('GILQueazyBundle:Visitor:accueil.html.twig',
+            array( 'quizs' => $listeQuiz )
+        );
     }
 }
--- queazy/src/GIL/QueazyBundle/Controller/Visitor/ListeQuizController.php
+++ queazy/src/GIL/QueazyBundle/Controller/Visitor/ListeQuizController.php
@@ -6,30 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ListeQuizController extends Controller
 {
-
-    private $offset;
-    private $currentPage;
+    private $limit = 10;
 
     public function listeAction($page = 1)
     {
         // 1. Aller chercher les quiz dans la BDD
         $bdd = $this->getDoctrine()->getManager();
-        /*
+
+        // Calcul de l'offset en fonction du nombre de
+
         $listeQuiz = $bdd->getRepository('GILQueazyBundle:Quiz')->findBy(
             null,
             array('date' => 'asc'),
-            array('limit' => 10),
-            array('offset' => 0)
+            array('limit' => $this->limit),
+            array('offset' => $offset)
         );
-        */
 
         // 2. Choisir le nombre de quiz à afficher par pages
         // 3. retourner les quiz correspondant à la page en cours.
-        /*
         return $this->render('GILQueazyBundle:Visitor:accueil.html.twig', array(
-            "listeQuiz" => listeQuiz
+            "listeQuiz" => $listeQuiz
         ));
-        */
-       return $this->render('GILQueazyBundle:Visitor:accueil.html.twig');
     }
 }
